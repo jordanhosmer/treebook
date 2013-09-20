efe5dc17c00e05f1ce814e320cd72403c8e2840a
Jordan Hosmer <jordanhosmer@gmail.com>
2013-09-20 00:07:22 -0700

    Gemfile update to match tutorial

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Treebook
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = true 
-
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile= true
   end
 end
